d0b243577dc0972d2bcae14cf23c637bb21ea0e2
safaalaloosi <salaloosii@gmail.com>
2020-11-10 09:26:32 -0800

    added new java class Hello,added a comment into main

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello");
      // B21  is good at IntelliJ
-
+     // mac user users are happy to see commit dialog finally
 
     }
 
